9e7cb1fbe12ec7ecb5c2fc08181874b576f39393
Thusitha Herath <83418246+ThusithaHerath@users.noreply.github.com>
2023-06-25 08:41:25 +0530

    merge conflicts

--- resources/views/modal/publisher.blade.php
+++ resources/views/modal/publisher.blade.php
@@ -1,57 +1,113 @@
-<!-- Modal -->
-<div class="modal fade bd-example-modal-lg text-black" id="publisher"  role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalCenterTitle">New Publisher</h5>
-        </div>
-        <form action="{{url('http://127.0.0.1:8000/api/publisher/store')}}" method="POST" enctype="multipart/form-data">
-          @csrf
-        <div class="modal-body text-black">
-            <div class="row mt-2">
-                <div class="col">
-                    <label class="mb-2" >Publisher:</label>
-                    <input type="text" name="publisher" class="form-control" placeholder="Publisher Name" required>
-                  </div>
-                <div class="col">
-                  <label class="mb-2" >Type:</label>
-                  <select class="form-select" name="type" aria-label="Default select example" required>
-                    <option selected>Select type</option>
-                    @foreach ($type as $type)
-                    <option value="{{$type->id}}">{{$type->PublisherType_Name}}</option>
-                    @endforeach
-                  </select>
-                </div>
-            </div>
+  <div class="modal fade" id="PublisherModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-lg modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content modalBody">
+              <div class="modal-header border-0">
+                  <h5 class="modal-title" id="exampleModalLabel">
+                      <h2 style="letter-spacing: 2px;font-size: 40px;" class="fw-bold">Publisher</h2>
+                  </h5>
+                  <button type="button" class="btn-close rounded-5" data-bs-dismiss="modal" aria-label="Close"
+                      style="background-color: rgba(128, 128, 128, 0.575);font-size:12px"></button>
+              </div>
+              <div class="modal-body">
+                  <!-- =======  modal Section ======= -->
+                  <section id="testimonials" class="pb-0 pt-0 rounded-3 section-bg testimonials"
+                      style="box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;">
+                      <div class="container" data-aos="fade-up">
 
-            <div class="row mt-2">
-                <div class="col">
-                    <label class="mb-2" >Simultaneous Submission OK *:</label>
-                    <select class="form-select" name="simultaneoussubmission" aria-label="Default select example" required>
-                    <option selected>Unknown</option>
-                    <option value="1">Yes</option>
-                    <option value="0">No</option>
-                    </select>
-                </div>
-                <div class="col">
-                    <label class="mb-2" >Rank:</label>
-                    <input type="text" name="rank" class="form-control" placeholder="Rank" required>
-                </div>
-                <div class="col">
-                    <label class="mb-2" >Deadline:</label>
-                    <input type="date" name="deadline" class="form-control" placeholder="Deadline" required>
-                </div>
-                <div class="col">
-                    <label class="mb-2" >Contact:</label>
-                    <input type="number" name="contact" class="form-control" placeholder="Contact" required>
-                </div>
-            </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-          <button type="submit" class="btn btn-primary">Save</button>
-        </div>
-      </form>
+                          <div class="testimonials-slider swiper mb-3" data-aos="fade-up" data-aos-delay="100">
+                              <div class="d-flex row">
+                                  <div class="col-12">
+
+                                      <div class="testimonial-item p-4 rounded-3">
+
+                                          <section id="book-a-table" class="book-a-table p-0">
+                                              <div class="container" data-aos="fade-up">
+                                                  <form action=" " role="form" class="php-email-form"
+                                                      data-aos="fade-up" data-aos-delay="100">
+
+
+                                                      <div class="d-flex row">
+                                                          <div class="form-group col-8">
+                                                              <label for="" class="form-label">Publisher</label>
+                                                              <input type="text" class="form-control"
+                                                                  id="data_publisher">
+                                                          </div>
+                                                          <div class="form-group col-4">
+                                                              <label for="" class="form-label">Type</label>
+                                                              <select class="single-select-publishernew" multiple
+                                                                  id="data_type" style="width:200px!important">
+                                                                  <option value="N/A">N/A</option>
+                                                                  <option value="AL">Alabama</option>
+                                                                  <option value="WY">Wyoming</option>
+                                                              </select>
+
+                                                          </div>
+
+
+                                                      </div>
+                                                      <div class="d-flex mb-3 row">
+                                                          <div class="form-group col-3">
+                                                              <label for="" class="form-label">Simultaneous
+                                                                  Submission OK? <span class="text-danger">*</span>
+                                                              </label>
+                                                              <select class="single-select-publishernew" multiple
+                                                                  id="data_submission" style="width:150px!important;">
+                                                                  <option value="Unknown">Unknown</option>
+                                                                  <option value="yes">Yes</option>
+                                                                  <option value="no">No</option>
+                                                                  <option value="all">Show All</option>
+                                                              </select>
+                                                          </div>
+                                                          <div class="form-group col-3 mt-4">
+                                                              <label for="" class="form-label">Rank</label>
+                                                              <input type="number" class="form-control" id="data_rank">
+                                                          </div>
+                                                          <div class="form-group col-3 mt-4">
+                                                              <label for="" class="form-label">Deadline</label>
+                                                              <input type="text" class="form-control"
+                                                                  id="data_deadline">
+                                                          </div>
+                                                          <div class="form-group col-3 mt-4">
+                                                              <label for="" class="form-label">Contact</label>
+                                                              <input type="text" class="form-control"
+                                                                  id="data_contact" maxlength="16">
+                                                          </div>
+
+                                                      </div>
+                                                      <span class="position-absolute">Mandotory Field <span
+                                                              class="text-danger">*</span></span>
+                                                      <div class="d-flex justify-content-end mt-4">
+                                                          <div class="text-center"><button type="submit"
+                                                                  class="bg-transparent border me-3"data-bs-dismiss="modal">Cancel</button>
+                                                          </div>
+                                                          <div class="text-center"><button type="submit">Save</button>
+                                                          </div>
+                                                      </div>
+
+                                                  </form>
+                                              </div>
+                                          </section><!-- End Book A Table Section -->
+
+
+
+                                      </div>
+                                  </div>
+
+
+                              </div>
+                          </div>
+
+
+
+
+
+
+                          </form>
+                      </div>
+                  </section><!-- End modal Section -->
+
+              </div>
+
+          </div>
       </div>
-    </div>
   </div>
--- resources/views/modal/story.blade.php
+++ resources/views/modal/story.blade.php
@@ -1,55 +1,112 @@
-<!-- Modal -->
-<div class="modal fade text-black" id="story"  role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalCenterTitle">New Story</h5>
-        </div>
-        <form  action="{{url('http://127.0.0.1:8000/api/story/store')}}" method="POST" enctype="multipart/form-data">
-          @csrf
-        <div class="modal-body text-black">
-                <div class="form-group">
-                    <label class="mb-2" >Story Name:</label>
-                    <input type="text" name="story" class="form-control" placeholder="Story name" required>
-                </div>
-                <div class="row mt-2">
-                    <div class="col">
-                      <label class="mb-2" >Status:</label>
-                      <select class="form-select" name="status" aria-label="Default select example"  required>
-                        <option selected>Select status</option>
-                        @foreach ($story_status as $status)
-                        <option value="{{$status->StoryCompletionStatus_ID}}">{{$status->StoryCompletionStatus_Name}}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                    <div class="col">
-                      <label class="mb-2" >Type:</label>
-                      <select class="form-select" name="type" aria-label="Default select example" required>
-                        <option selected>Select type</option>
-                        @foreach ($story_type as $type)
-                        <option value="{{$type->StoryType_ID}}">{{$type->StoryType_Name}}</option>
-                        @endforeach
-                      </select>
-                    </div>
-                    <div class="col">
-                      <label class="mb-2" >Lines:</label>
-                      <input type="number" name="lines" class="form-control" placeholder="Lines" required>
-                    </div>
-                    <div class="col">
-                      <label class="mb-2" >Words:</label>
-                      <input type="number" name="words" class="form-control" placeholder="Words" required>
-                    </div>
-                </div>
-                <div class="form-group">
-                  <label class="mb-2" >Summary</label>
-                  <textarea class="form-control" name="summary" id="exampleFormControlTextarea1" rows="3"></textarea>
+      <div class="modal fade" id="storyModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+              <div class="modal-content modalBody">
+                  <div class="modal-header border-0">
+                      <h5 class="modal-title" id="exampleModalLabel">
+                          <h2 style="letter-spacing: 2px;font-size: 40px;" class="fw-bold">Story</h2>
+                      </h5>
+                      <button type="button" class="btn-close rounded-5" data-bs-dismiss="modal" aria-label="Close"
+                          style="background-color: rgba(128, 128, 128, 0.575);font-size:12px"></button>
+                  </div>
+                  <div class="modal-body">
+                      <!-- =======  modal Section ======= -->
+                      <section id="testimonials" class="pb-0 pt-0 rounded-3 section-bg testimonials"
+                          style="box-shadow: rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px;">
+                          <div class="container" data-aos="fade-up">
+
+                              <div class="testimonials-slider swiper mb-3" data-aos="fade-up" data-aos-delay="100">
+                                  <div class="d-flex row">
+                                      <div class="col-12">
+
+                                          <div class="testimonial-item p-4 rounded-3">
+
+                                              <section id="book-a-table" class="book-a-table p-0">
+                                                  <div class="container" data-aos="fade-up">
+                                                      <form action=" " role="form" class="php-email-form"
+                                                          data-aos="fade-up" data-aos-delay="100">
+                                                          <div class="form-group mb-3">
+                                                              <label for="" class="form-label">Story</label>
+                                                              <input type="text" class="form-control"
+                                                                  id="data_story">
+                                                          </div>
+
+                                                          <div class="d-flex">
+                                                              <div class="form-group me-5">
+                                                                  <label for=""
+                                                                      class="form-label">Status</label>
+                                                                  <select class="single-select-storyNew" multiple
+                                                                      style="width:150px!important" id="data_status">
+                                                                      <option value="N/A">N/A</option>
+                                                                      <option value="hold">Alabama</option>
+                                                                      <option value="WY">Wyoming</option>
+                                                                  </select>
+                                                              </div>
+                                                              <div class="form-group me-2 p-0">
+                                                                  <label for="" class="form-label">Type</label>
+                                                                  <select class="single-select-storyNew" multiple
+                                                                      id="data__type" style="width:150px!important">
+                                                                      <option value="N/A">N/A</option>
+                                                                      <option value="AL">Alabama</option>
+                                                                      <option value="WY">Wyoming</option>
+                                                                  </select>
+
+                                                              </div>
+
+
+                                                          </div>
+                                                          <div class="d-flex mb-3">
+                                                              <div class="form-group me-5">
+                                                                  <label for="" class="form-label">Lines</label>
+                                                                  <input type="number"
+                                                                      class="form-control"style="width:150px!important"
+                                                                      id="data_lines">
+                                                              </div>
+                                                              <div class="form-group me-2 p-0">
+                                                                  <label for="" class="form-label">Words</label>
+                                                                  <input type="number"
+                                                                      class="form-control"style="width:150px!important"
+                                                                      id="data_words">
+                                                              </div>
+
+
+                                                          </div>
+                                                          <div class="form-group  mb-4">
+                                                              <label for="" class="form-label">Summary</label>
+                                                              <textarea class="form-control" name="message" rows="5" id="data_summary" placeholder="Type here..."></textarea>
+
+                                                          </div>
+                                                          <div class="d-flex justify-content-end mt-3">
+                                                              <div class="text-center"><button type="submit"
+                                                                      class="bg-transparent border me-3"data-bs-dismiss="modal">Cancel</button>
+                                                              </div>
+                                                              <div class="text-center"><button
+                                                                      type="submit">Save</button></div>
+                                                          </div>
+
+                                                      </form>
+                                                  </div>
+                                              </section><!-- End Book A Table Section -->
+
+
+
+                                          </div>
+                                      </div>
+
+
+                                  </div>
+                              </div>
+
+
+
+
+
+
+                              </form>
+                          </div>
+                      </section><!-- End modal Section -->
+
+                  </div>
+
               </div>
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
-          <button type="submit" class="btn btn-primary">Save</button>
-        </div>
-      </form>
+          </div>
       </div>
-    </div>
-  </div>
